c8e0d19beaa27394f4e1464aedd99a756d0b0c48
nickicn <nicolelopes0008@gmail.com>
2025-08-26 11:40:49 -0300

    Configuração dos serviços da aplicação:)

--- LNF.API/Program.cs
+++ LNF.API/Program.cs
@@ -1,8 +1,68 @@
+using System.Text;
+using LNF.API.Data;
+using LNF.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ZstdSharp.Unsafe;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Serviço de conexão com o banco
+string conexao = builder.Configuration.GetConnectionString("Conexao");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL(conexao)
+);
+
+// Serviço de Autenticação e Autorização - Identity
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    // Configurar Senha
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+
+
+    // Configurações de Bloqueio
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+    // Configação Usuário
+    options.User.RequireUniqueEmail = true;
+
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// Serviço JWT
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+})
+.AddJwtBearer(options =>
+{
+     options.TokenValidationParameters = new()
+     {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey =  new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+     };
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,12 +76,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI( c => 
+    app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "LNF v1");
         c.RoutePrefix = string.Empty;
     }
-    );             
+    );
 }
 
 app.UseHttpsRedirection();
