c7268cda7ede1fed9db8c1bb7e9231d2cb4db3c3
nickicn <nicolelopes0008@gmail.com>
2025-08-29 11:35:54 -0300

    Configurando Serviços :))

--- LNF.API/Program.cs
+++ LNF.API/Program.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using ZstdSharp.Unsafe;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -63,18 +64,63 @@ builder.Services.AddAuthentication(options =>
      };
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Adicionar a Autorização
+builder.Services.AddAuthorization();
+
+// Registro dos Serviçes Customizados
+
+// Registro dos Repositórios
+
+// Registro do Seviços
+
+// Configuração dos CORS
+builder.Services.AddCors( options => 
+{
+    options.AddPolicy("AllowAll", policy => 
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo{
+        Title = "LNF API",
+        Version = "v1",
+        Description = "API para gerenciamento de finanças pessoais"
+     });
+     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+     {
+        Description = "Cabeçalho da Autorização JWT. Exemplo: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+     });
+
+     c.AddSecurityRequirement(new OpenApiSecurityRequirement
+     {
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+     });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
@@ -84,7 +130,12 @@ if (app.Environment.IsDevelopment())
     );
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
